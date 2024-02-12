4fd8ab50f706a63844abbbe689ad6e356ec185ac
Kamazhay <qwertykama29@gmail.com>
2024-02-12 19:51:07 +0600

    finished project (lab2)

---
+++ app/src/main/java/com/example/project_kama/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.project_kama
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnBio= findViewById<Button>(R.id.button2)
+        btnBio.setOnClickListener {
+            intent= Intent(applicationContext,secondscreenkama::class.java)
+            startActivity(intent)
+        }
+        val webButton= findViewById<Button>(R.id.button3)
+        webButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse("https://www.instagram.com/29kama")
+            startActivity(intent)
+        }
+        val CameraButton= findViewById<Button>(R.id.button4)
+        CameraButton.setOnClickListener {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivity(intent)
+        }
+    }
+}
